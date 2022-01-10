b7e23e63a441e73cf6a5b9839a35a8e0d0df2576
Misha Causur <mishacausur@mail.ru>
2022-01-11 00:14:21 +0500

    fields and card are ready

--- Meedies/ContentView.swift
+++ Meedies/ContentView.swift
@@ -6,11 +6,69 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
-        Text("Select an item")
+        ZStack {
+            Image("backgroundMax")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                VStack(alignment: .leading, spacing: 16) {
+                    Text("Sign up")
+                        .font(.title.bold())
+                        .foregroundColor(.white)
+                    Text("Get the all new data from around the world in the one place")
+                        .font(.subheadline)
+                        .foregroundColor(.white).opacity(0.7)
+                    HStack(spacing: 12) {
+                        Image(systemName: "envelope")
+                            .foregroundColor(.white)
+                            .padding(.leading, 12)
+                        TextField("Email", text: $email)
+                            .colorScheme(.dark)
+                            .foregroundColor(.white.opacity(0.8))
+                            .autocapitalization(.none)
+                            .textContentType(.emailAddress)
+                    }
+                    .frame(height: 42)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 30, style: .continuous)
+                            .stroke(Color.white, lineWidth: 0.3)
+                            .blendMode(.overlay))
+                    
+                    HStack(spacing: 12) {
+                        Image(systemName: "key")
+                            .foregroundColor(.white)
+                            .padding(.leading, 12)
+                        SecureField("Password", text: $password)
+                            .colorScheme(.dark)
+                            .foregroundColor(.white.opacity(0.8))
+                            .autocapitalization(.none)
+                            .textContentType(.password)
+                    }
+                    .frame(height: 42)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 30, style: .continuous)
+                            .stroke(Color.white, lineWidth: 0.3)
+                            .blendMode(.overlay))
+                }
+                
+                .padding(20)
+            }
+            .background(
+                RoundedRectangle(cornerRadius: 30, style: .continuous)
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(30)
+                    .shadow(color: Color("shadowColor").opacity(0.5), radius: 60, x: 0, y: 30)
+                    )
+            .background(
+                RoundedRectangle(cornerRadius: 30, style: .continuous).stroke(Color.white).opacity(0.7))
+            .padding(.horizontal, 10)
+        }
     }
 }
 
